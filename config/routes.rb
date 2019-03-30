36d2d5b6fbf9d1e11ff0603c0dd48d63085bb5c2
Kevin Coleman <kevin.coleman@sparkstart.io>
2019-03-30 16:07:41 -0400

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/:id', to: 'trees#show'
+  get '/:id/parent/:parent_id', to: 'trees#parent'
+  get '/:id/child/:child_id', to: 'trees#child'
 end
